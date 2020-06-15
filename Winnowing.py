3790246038bf78d76b79207d5d3b67440f79e8bf
jmmarotta <julianmmarotta@gmail.com>
2020-06-15 14:13:22 -0400

    Rolling Hash and Similarity Created a rolling hash function and similarity.

--- Winnowing.py
+++ Winnowing.py
@@ -1,18 +1,15 @@
-import hashlib
 import re
 import sys
-from cryptography.fernet import Fernet
 
 
+# Setup the winnowing function by removing all common characters and retrieving the k-gram hashes
 def winnow_setup(text, k, w):
+    # text to lowercase and remove all non-alphanumerics for text
     text = text.lower()
     text = re.sub(r'\W+', '', text)
-    grams = []
-    for i in range(0, len(text)-k+1):
-        grams.append(text[i:i+k])
-    hashes = []
-    for gram in grams:
-        hashes.append(int(hashlib.md5(gram.encode('UTF-8')).hexdigest(), 16))
+    # retrieve the k-gram hashes
+    hashes = compute_hash(text, k)
+    # return the output of the winnow function
     return winnow(w, hashes)
 
 
@@ -20,37 +17,48 @@ def winnow_setup(text, k, w):
 def winnow(w, hashes):
     recorded = {}
     h2 = hashes.copy()
+    # create the window of size 4
     h = [sys.maxsize for i in range(0, w)]
     r = 0
     minimum = 0
     global_pos = 0
-    for i in range(0, len(hashes)-1):
+    # loop through the hashes to find the minimum hash in every window
+    for i in range(0, len(hashes)):
         r = (r + 1) % w
         h[r] = h2.pop(0)
+        # if the minimum is the current index, check entire window for the minimum
         if minimum == r:
             for ind in scan_left_ind(r, w):
                 if h[ind] < h[minimum]:
                     minimum = ind
             recorded = record(recorded, h[minimum], global_pos, w)
-        else:
-            if h[r] <= h[minimum]:
+        else:  # check if the current index is the new minimum
+            if h[r] < h[minimum]:
                 minimum = r
                 recorded = record(recorded, h[minimum], global_pos, w)
         global_pos += 1
     return recorded
 
 
+# record the current hash and the its positioning
 def record(recorded, minimum, global_pos, w):
+    # determine if there is another hash in the same window already
     if global_pos < w and len(recorded) > 0:
         for rec in recorded:
+            # if there is, determine the true minimum and record it
             if minimum <= rec:
                 recorded.pop(rec)
-                recorded[minimum] = global_pos
+                recorded[minimum] = [global_pos]
     else:
-        recorded[minimum] = global_pos
+        if minimum in recorded:
+            recorded[minimum] = recorded[minimum] + [global_pos]
+        else:
+            recorded[minimum] = [global_pos]
     return recorded
 
 
+# create an array starting at the rightmost index of the current window
+# continue until you hit the current index, r
 def scan_left_ind(r, w):
     inds = []
     step = (r - 1) % w
@@ -60,24 +68,68 @@ def scan_left_ind(r, w):
     return inds
 
 
-def compare_documents(file1, file2):
-    f = open(file1, "r")
-    txt = f.read()
-    fingerprints1 = winnow_setup(txt, 10, 4)
+# compute the k-gram hashes through a rolling hash function
+def compute_hash(s, k):
+    # setup the compute hash function
+    ints = compute_ints(s)
+    p = 31
+    m = 10 ** 9 + 9
+    # compute the p_pow values
+    p_pow = compute_p_pow(k, p, m)
+    final_pow = p_pow[k - 1]
+    # compute the initial hash value
+    hashes = [sum([num * power % m for num, power in zip(ints[0:k], p_pow)])]
+    for i in range(0, len(s) - k):
+        # compute the next hash value through the previous one
+        hashes.append(int((hashes[i] - ints[i]) / p % m + (ints[k + i] * final_pow) % m))
+    return hashes
 
-    f2 = open(file2, "r")
-    txt2 = f2.read()
-    fingerprints2 = winnow_setup(txt2, 10, 4)
+
+# return the modified int value for the characters in the text
+def compute_ints(s):
+    ints = []
+    for ch in s:
+        ints.append(ord(ch) - ord('a') + 1)
+    return ints
+
+
+# compute the p_pow values
+def compute_p_pow(k, p, m):
+    p_pow = [1]
+    for i in range(1, k):
+        p_pow.append((p_pow[i-1] * p) % m)
+    return p_pow
+
+
+def compare_files(student_file_loc, base_file_loc):
+    student_file = open(student_file_loc, "r")
+    student_txt = student_file.read()
+    student_fingerprints = winnow_setup(student_txt, 10, 4)
+    num_std_fps = 0
+    for val in student_fingerprints.values():
+        for num in val:
+            num_std_fps += 1
+
+    base_file = open(base_file_loc, "r")
+    base_txt = base_file.read()
+    base_fingerprints = winnow_setup(base_txt, 10, 4)
 
     common = []
-    for fp in list(fingerprints1.keys()):
-        if fp in list(fingerprints2.keys()):
+    num_common_fps = 0
+    for fp in list(student_fingerprints.keys()):
+        if fp in list(base_fingerprints.keys()):
             common.append(fp)
-    print("The documents have %d fingerprint(s) in common." % len(common))
+            for num in student_fingerprints[fp]:
+                num_common_fps += 1
+
+    similarity = num_common_fps / num_std_fps
+    plagiarized = "plagiarized" if similarity >= 0.25 else "not plagiarized"
+    print("The student file is {:.2%} similar to the base file.\n".format(similarity) +
+          "The student file was likely {}.".format(plagiarized))
 
 
 def main():
-    compare_documents("test.txt", "test2.txt")
+    compare_files("test.txt", "test2.txt")
 
 
 if __name__ == "__main__":
