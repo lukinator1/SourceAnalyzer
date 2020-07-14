9613020b8c9f6190831dcb423b5219165d19956e
lukinator1 <32145995+lukinator1@users.noreply.github.com>
2020-07-14 00:03:33 -0400

    stuff for frontend

--- build/lib/source/backend/file_to_fingerprint.py
+++ build/lib/source/backend/file_to_fingerprint.py
@@ -1,11 +1,6 @@
-# A filetofingerprint object has a filename, a unique fileid, an originality percentage (not implemented right now), a fingerprintssetup as [hash:locationinfile],
-# a similarto dictionary as {similarfileobject:[([originalfingerprintobjects],[similarfingerprintobjects])]}, and a mostimportantmatches
-# list dictionary as {mostimportantfilematch:[((originalfilestartfingerprint, originalfileendfingerprint), [(simfilestartfingerprint, simfileendfingerprint)])]}
-class filetofingerprint():
-    def __init__(self, filename, fileid, originality, fingerprintssetup, similarto, mostimportantmatches):
+class FileToFingerprint():
+    def __init__(self, filename, fileid, keys, similarto):
         self.filename = filename
         self.fileid = fileid
-        self.originality = originality
-        self.fingerprintssetup = fingerprintssetup
+        self.keys = keys
         self.similarto = similarto
-        self.mostimportantmatches = mostimportantmatches
--- hashedgrams.py
+++
@@ -1,5 +0,0 @@
-#unused for right now
-class hashedgrams():
-    def __init__(self, hash, string):
-        self.hash = hash
-        self.string = string
